551b15bb12949b4ec042345168ba727563dd96f9
Riaz Shageer <riaz.shageer@gmail.com>
2025-03-07 02:21:29 -0400

    fix: Create Guards to check for slint-viewer and pgrep

--- plugin/slint-preview.lua
+++ plugin/slint-preview.lua
@@ -2,3 +2,11 @@ if vim.fn.has("nvim-0.7.0") ~= 1 then
    vim.api.nvim_err_writeln("slint-preview.nvim requires at least nvim-0.7.0.")
 end
 
+if vim.fn.executable("slint-viewer") == 0 then
+   vim.api.nvim_err_writeln("slint-preview.nvim requires slint-viewer to be installed.")
+end
+
+if vim.fn.executable("pgrep") == 0 then
+   vim.api.nvim_err_writeln("slint-preview.nvim requires pgrep to be installed.")
+end
+
